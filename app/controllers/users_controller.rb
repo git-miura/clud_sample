0c2f760a94b8650320fbac10fd98f91ddb36ff8a
三浦 <miura.mai910@gmail.com>
2021-05-07 16:49:34 +0900

    Rails課題（基礎）2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  #一覧画面に対応するアクション
+  def index
+    @users = User.all
+  end
+
+  #新規作成画面に対応するアクション
+  def new
+    @user = User.new
+  end
+
+  #新規登録するためのアクション
+  def create
+    User.create(user_params)
+    redirect_to action: :index
+  end
+
+  #更新を行うためのアクションゲーム
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to action: :index
+  end
+
+  #削除するためのアクション
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+    redirect_to action: :index
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  #Strong Parameter データベースに登録するデータを制限
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :age)
+  end
+end
